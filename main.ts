842e4fc58d77af9d57313eaacf7b8934bd256ad5
benthebiesmans <benthebiesmans@gmail.com>
2024-11-25 15:05:39 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,10 +1,1 @@
-let getal = 0
-basic.forever(function () {
-    basic.showNumber(getal)
-    if (input.buttonIsPressed(Button.A)) {
-        getal += 1
-    } else if (input.buttonIsPressed(Button.B)) {
-        basic.showNumber(getal)
-        getal += -1
-    }
-})
+
